44228a67b4907e44221e76dd636f51f1c0476e4e
immortalqx <immortalqx@gmail.com>
2024-05-20 17:45:07 +0800

    update

--- scene/__init__.py
+++ scene/__init__.py
@@ -18,18 +18,34 @@ from scene.gaussian_model import GaussianModel
 from arguments import ModelParams
 from utils.camera_utils import cameraList_from_camInfos, camera_to_JSON
 
+
 class Scene:
+    """
+    Scene 类用于管理场景的3D模型, 包括相机参数、点云数据和高斯模型的初始化和加载
+    """
+    # 表示 gaussians 应该是 GaussianModel 类的实例
+    # Python 3.6 及以上版本引入的类型提示（type hints）语法，这个更像是一个规范约束，并不是一个语法限制
+    gaussians: GaussianModel
 
-    gaussians : GaussianModel
-
-    def __init__(self, args : ModelParams, gaussians : GaussianModel, load_iteration=None, shuffle=True, resolution_scales=[1.0]):
-        """b
-        :param path: Path to colmap scene main folder.
+    def __init__(self, args: ModelParams, gaussians: GaussianModel,
+                 load_iteration=None, shuffle=True, resolution_scales=[1.0]):
         """
+        初始化场景对象
+        :param args: 包含模型路径和源路径等模型参数
+        :param gaussians: 高斯模型对象,用于场景点的3D表示
+        :param load_iteration: 指定加载模型的迭代次数，如果为-1,则自动寻找最大迭代次数
+        :param shuffle: 是否在训练前打乱相机列表
+        :param resolution_scales: 分辨率比例列表，用于处理不同分辨率的相机
+        """
+
+        # 模型文件保存路径
         self.model_path = args.model_path
+        # 已加载的迭代次数
         self.loaded_iter = None
+        # 高斯模型对象
         self.gaussians = gaussians
 
+        # 检查并加载已有的训练模型
         if load_iteration:
             if load_iteration == -1:
                 self.loaded_iter = searchForMaxIteration(os.path.join(self.model_path, "point_cloud"))
@@ -37,9 +53,13 @@ class Scene:
                 self.loaded_iter = load_iteration
             print("Loading trained model at iteration {}".format(self.loaded_iter))
 
+        # 用于训练的相机参数
         self.train_cameras = {}
+        # 用于测试的相机参数
         self.test_cameras = {}
 
+        # 根据数据集类型（COLMAP或Blender）加载场景信息
+        # 加载场景信息的函数sceneLoadTypeCallbacks，定义在scene/dataset_readers.py中
         if os.path.exists(os.path.join(args.source_path, "sparse")):
             scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
@@ -48,8 +68,10 @@ class Scene:
         else:
             assert False, "Could not recognize scene type!"
 
+        # 如果是初次训练，初始化3D高斯模型；否则，加载已有模型
         if not self.loaded_iter:
-            with open(scene_info.ply_path, 'rb') as src_file, open(os.path.join(self.model_path, "input.ply") , 'wb') as dest_file:
+            with open(scene_info.ply_path, 'rb') as src_file, open(os.path.join(self.model_path, "input.ply"),
+                                                                   'wb') as dest_file:
                 dest_file.write(src_file.read())
             json_cams = []
             camlist = []
@@ -68,25 +90,39 @@ class Scene:
 
         self.cameras_extent = scene_info.nerf_normalization["radius"]
 
+        # 根据resolution_scales加载不同分辨率的训练和测试位姿
         for resolution_scale in resolution_scales:
             print("Loading Training Cameras")
-            self.train_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.train_cameras, resolution_scale, args)
+            self.train_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.train_cameras, resolution_scale,
+                                                                            args)
             print("Loading Test Cameras")
-            self.test_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.test_cameras, resolution_scale, args)
+            self.test_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.test_cameras, resolution_scale,
+                                                                           args)
 
+        # 如果已加载模型，则调用 load_ply 方法加载点云数据
         if self.loaded_iter:
             self.gaussians.load_ply(os.path.join(self.model_path,
-                                                           "point_cloud",
-                                                           "iteration_" + str(self.loaded_iter),
-                                                           "point_cloud.ply"))
+                                                 "point_cloud",
+                                                 "iteration_" + str(self.loaded_iter),
+                                                 "point_cloud.ply"))
+        # 否则，调用 create_from_pcd 方法根据场景信息中的点云数据创建高斯模型
         else:
             self.gaussians.create_from_pcd(scene_info.point_cloud, self.cameras_extent)
 
     def save(self, iteration):
+        """
+        保存当前迭代下的3D高斯模型点云。
+        :param iteration: 当前的迭代次数。
+        """
         point_cloud_path = os.path.join(self.model_path, "point_cloud/iteration_{}".format(iteration))
         self.gaussians.save_ply(os.path.join(point_cloud_path, "point_cloud.ply"))
 
     def getTrainCameras(self, scale=1.0):
+        """
+        获取指定分辨率比例的训练相机列表
+        :param scale: 分辨率比例
+        :return: 指定分辨率比例的训练相机列表
+        """
         return self.train_cameras[scale]
 
     def getTestCameras(self, scale=1.0):
--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -23,6 +23,7 @@ from plyfile import PlyData, PlyElement
 from utils.sh_utils import SH2RGB
 from scene.gaussian_model import BasicPointCloud
 
+
 class CameraInfo(NamedTuple):
     uid: int
     R: np.array
@@ -35,12 +36,14 @@ class CameraInfo(NamedTuple):
     width: int
     height: int
 
+
 class SceneInfo(NamedTuple):
     point_cloud: BasicPointCloud
     train_cameras: list
     test_cameras: list
     nerf_normalization: dict
     ply_path: str
+
 
 def getNerfppNorm(cam_info):
     def get_center_and_diag(cam_centers):
@@ -65,44 +68,61 @@ def getNerfppNorm(cam_info):
 
     return {"translate": translate, "radius": radius}
 
+
 def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
+    # 初始化用于存储相机信息的列表
     cam_infos = []
+    # 遍历所有相机的外参
     for idx, key in enumerate(cam_extrinsics):
         sys.stdout.write('\r')
+        # 动态显示读取相机信息的进度
         # the exact output you're looking for:
-        sys.stdout.write("Reading camera {}/{}".format(idx+1, len(cam_extrinsics)))
+        sys.stdout.write("Reading camera {}/{}".format(idx + 1, len(cam_extrinsics)))
         sys.stdout.flush()
 
+        # 获取当前相机的外参和内参
         extr = cam_extrinsics[key]
         intr = cam_intrinsics[extr.camera_id]
         height = intr.height
         width = intr.width
 
         uid = intr.id
+        # 将四元数表示的旋转转换为旋转矩阵R
         R = np.transpose(qvec2rotmat(extr.qvec))
+        # 外参中的平移向量
         T = np.array(extr.tvec)
 
-        if intr.model=="SIMPLE_PINHOLE":
+        # 根据相机内参模型计算视场角（FoV)
+        if intr.model == "SIMPLE_PINHOLE":
+            # 如果是简单针孔模型，只有一个焦距参数
             focal_length_x = intr.params[0]
-            FovY = focal2fov(focal_length_x, height)
-            FovX = focal2fov(focal_length_x, width)
-        elif intr.model=="PINHOLE":
+            FovY = focal2fov(focal_length_x, height)  # 计算垂直方向的视场角
+            FovX = focal2fov(focal_length_x, width)  # 计算水平方向的视场角
+        elif intr.model == "PINHOLE":
+            # 如果是针孔模型，有两个焦距参数
             focal_length_x = intr.params[0]
             focal_length_y = intr.params[1]
-            FovY = focal2fov(focal_length_y, height)
-            FovX = focal2fov(focal_length_x, width)
+            FovY = focal2fov(focal_length_y, height)  # 计算垂直方向的视场角
+            FovX = focal2fov(focal_length_x, width)  # 计算水平方向的视场角
         else:
             assert False, "Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!"
 
+        # 构建图片的完整路径
         image_path = os.path.join(images_folder, os.path.basename(extr.name))
+        # 提取图片名称，不包含扩展名
         image_name = os.path.basename(image_path).split(".")[0]
+        # 使用PIL库打开图片文件
         image = Image.open(image_path)
 
+        # 创建并存储相机信息
         cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
                               image_path=image_path, image_name=image_name, width=width, height=height)
         cam_infos.append(cam_info)
+    # 在读取完所有相机信息后换行
     sys.stdout.write('\n')
+    # 返回整理好的相机信息列表
     return cam_infos
+
 
 def fetchPly(path):
     plydata = PlyData.read(path)
@@ -112,12 +132,13 @@ def fetchPly(path):
     normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
+
 def storePly(path, xyz, rgb):
     # Define the dtype for the structured array
     dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
-            ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
-            ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
-    
+             ('nx', 'f4'), ('ny', 'f4'), ('nz', 'f4'),
+             ('red', 'u1'), ('green', 'u1'), ('blue', 'u1')]
+
     normals = np.zeros_like(xyz)
 
     elements = np.empty(xyz.shape[0], dtype=dtype)
@@ -129,31 +150,44 @@ def storePly(path, xyz, rgb):
     ply_data = PlyData([vertex_element])
     ply_data.write(path)
 
+
+# 此函数用来处理colmap格式的数据输入
 def readColmapSceneInfo(path, images, eval, llffhold=8):
     try:
+        # 尝试读取COLMAP处理结果中的二进制相机外参和内参文件
         cameras_extrinsic_file = os.path.join(path, "sparse/0", "images.bin")
         cameras_intrinsic_file = os.path.join(path, "sparse/0", "cameras.bin")
         cam_extrinsics = read_extrinsics_binary(cameras_extrinsic_file)
         cam_intrinsics = read_intrinsics_binary(cameras_intrinsic_file)
     except:
+        # 如果二进制文件读取失败，尝试读取文本格式的相机外参和内参文件
         cameras_extrinsic_file = os.path.join(path, "sparse/0", "images.txt")
         cameras_intrinsic_file = os.path.join(path, "sparse/0", "cameras.txt")
         cam_extrinsics = read_extrinsics_text(cameras_extrinsic_file)
         cam_intrinsics = read_intrinsics_text(cameras_intrinsic_file)
 
+    # 定义存放图片的目录，如果未指定则默认为"images"
     reading_dir = "images" if images == None else images
-    cam_infos_unsorted = readColmapCameras(cam_extrinsics=cam_extrinsics, cam_intrinsics=cam_intrinsics, images_folder=os.path.join(path, reading_dir))
-    cam_infos = sorted(cam_infos_unsorted.copy(), key = lambda x : x.image_name)
+    # 读取并处理相机参数，转换为内部使用的格式
+    cam_infos_unsorted = readColmapCameras(cam_extrinsics=cam_extrinsics, cam_intrinsics=cam_intrinsics,
+                                           images_folder=os.path.join(path, reading_dir))
+    # 根据图片名称对相机信息进行排序，以保证顺序一致性
+    cam_infos = sorted(cam_infos_unsorted.copy(), key=lambda x: x.image_name)
 
+    # 根据是否为评估模式（eval），将相机分为训练集和测试集
     if eval:
+        # 如果为评估模式，根据llffhold参数（通常用于LLFF数据集）间隔选择测试相机
         train_cam_infos = [c for idx, c in enumerate(cam_infos) if idx % llffhold != 0]
         test_cam_infos = [c for idx, c in enumerate(cam_infos) if idx % llffhold == 0]
     else:
+        # 如果不是评估模式，所有相机均为训练相机，测试相机列表为空
         train_cam_infos = cam_infos
         test_cam_infos = []
 
+    # 计算场景归一化参数，这是为了处理不同尺寸和位置的场景，使模型训练更稳定
     nerf_normalization = getNerfppNorm(train_cam_infos)
 
+    # 尝试读取点云数据，优先从PLY文件读取，如果不存在，则尝试从BIN或TXT文件转换并保存为PLY格式
     ply_path = os.path.join(path, "sparse/0/points3D.ply")
     bin_path = os.path.join(path, "sparse/0/points3D.bin")
     txt_path = os.path.join(path, "sparse/0/points3D.txt")
@@ -169,12 +203,14 @@ def readColmapSceneInfo(path, images, eval, llffhold=8):
     except:
         pcd = None
 
+    # 组装场景信息，包括点云、训练用相机、测试用相机、场景归一化参数和点云文件路径
     scene_info = SceneInfo(point_cloud=pcd,
                            train_cameras=train_cam_infos,
                            test_cameras=test_cam_infos,
                            nerf_normalization=nerf_normalization,
                            ply_path=ply_path)
     return scene_info
+
 
 def readCamerasFromTransforms(path, transformsfile, white_background, extension=".png"):
     cam_infos = []
@@ -194,7 +230,7 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
 
             # get the world-to-camera transform and set R, T
             w2c = np.linalg.inv(c2w)
-            R = np.transpose(w2c[:3,:3])  # R is stored transposed due to 'glm' in CUDA code
+            R = np.transpose(w2c[:3, :3])  # R is stored transposed due to 'glm' in CUDA code
             T = w2c[:3, 3]
 
             image_path = os.path.join(path, cam_name)
@@ -203,27 +239,29 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
 
             im_data = np.array(image.convert("RGBA"))
 
-            bg = np.array([1,1,1]) if white_background else np.array([0, 0, 0])
+            bg = np.array([1, 1, 1]) if white_background else np.array([0, 0, 0])
 
             norm_data = im_data / 255.0
-            arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
-            image = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+            arr = norm_data[:, :, :3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
+            image = Image.fromarray(np.array(arr * 255.0, dtype=np.byte), "RGB")
 
             fovy = focal2fov(fov2focal(fovx, image.size[0]), image.size[1])
-            FovY = fovy 
+            FovY = fovy
             FovX = fovx
 
             cam_infos.append(CameraInfo(uid=idx, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
-                            image_path=image_path, image_name=image_name, width=image.size[0], height=image.size[1]))
-            
+                                        image_path=image_path, image_name=image_name, width=image.size[0],
+                                        height=image.size[1]))
+
     return cam_infos
+
 
 def readNerfSyntheticInfo(path, white_background, eval, extension=".png"):
     print("Reading Training Transforms")
     train_cam_infos = readCamerasFromTransforms(path, "transforms_train.json", white_background, extension)
     print("Reading Test Transforms")
     test_cam_infos = readCamerasFromTransforms(path, "transforms_test.json", white_background, extension)
-    
+
     if not eval:
         train_cam_infos.extend(test_cam_infos)
         test_cam_infos = []
@@ -235,7 +273,7 @@ def readNerfSyntheticInfo(path, white_background, eval, extension=".png"):
         # Since this data set has no colmap data, we start with random points
         num_pts = 100_000
         print(f"Generating random point cloud ({num_pts})...")
-        
+
         # We create random points inside the bounds of the synthetic Blender scenes
         xyz = np.random.random((num_pts, 3)) * 2.6 - 1.3
         shs = np.random.random((num_pts, 3)) / 255.0
@@ -254,7 +292,8 @@ def readNerfSyntheticInfo(path, white_background, eval, extension=".png"):
                            ply_path=ply_path)
     return scene_info
 
+
 sceneLoadTypeCallbacks = {
     "Colmap": readColmapSceneInfo,
-    "Blender" : readNerfSyntheticInfo
+    "Blender": readNerfSyntheticInfo
 }
